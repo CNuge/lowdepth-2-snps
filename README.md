6b3738997e8c0e599c434ca25abab8f5499a45ef
Cam Nugent <nugentc@uoguelph.ca>
2021-11-18 14:48:47 -0400

    initial setup

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # lowdepth-2-snps
 A bioinformatics pipeline to process low depth re-sequencing data and produce SNP calls and genotype likelihoods
+
+## setup and use
+
+	1. clone repository
+
+	2. place data into the /data/ folder 
+		- your genome (.fna suffix) and paired fq files
+		- alternatively change the path in the /scripts/ files to interact with data in another location
+
+	3. open the checklist file
+		- alter slurm scripts as needed (increase time or cores if bigger datasets)
+		- make the job scheduler class
+
+	4. start the slurm job submission checklist
+
+
+
+## TODO
+[] bin scripts add
+[] slurm calls add
+[] change path imports on slurm scripts
+	- try to make things relative, so that the amount of hard coding is minimized
+[] work on checklist
+	- add info on what can be changed, 
+	- add some detail to what each step is doing.
+[] change scripts to save files to logs
+[] document "things you need to provide"
+	- genome file
+	- fq files
+	- a list of chr names
+	- see if others
+[]
+[]
---
+++ slurm_script_calls.sh
@@ -0,0 +1,58 @@
+
+############################
+# Slurm submission checklist
+############################
+
+
+#[] 0. preprocessing and data acquisitions
+## To alter in this slurm script:
+## 		-
+echo "housekeeping step, get all the files you want to work with in one directory"
+echo "this will involve a human in the loop"
+sbatch scripts/00_bad_sample_rm.sh
+
+
+#[] 1. clean fq files
+## To alter in this slurm script:
+## 		-
+echo "clean the data using cutadapt"
+echo "note this step may change due to use of alternative trim software"
+sbatch scripts/01_run_cutadapt.sh
+
+
+#[] 2. alignment to genome 
+## To alter in this slurm script:
+## 		-
+echo "bwa alignment of trimmed fq data to the genome"
+sbatch scripts/02_align_to_genome.sh
+
+
+#[] 3. deduplicate bam files
+## To alter in this slurm script:
+## 		-
+echo "bam file cleanup, deduplicate reads"
+sbatch scripts/03_deduplicate_and_cleanup.sh
+
+
+#[] 4. housekeeping
+## To alter in this slurm script:
+## 		-
+echo "bam file cleanup, alter the header information and index files. make genome dictonary"
+sbatch scripts/04_genome_dict_and_header_fix.sh
+
+
+#[] 5. indel realignment
+## To alter in this slurm script:
+## 		-
+echo "conduct the indel realignment"
+echo "with move to gatk4.0 this step may become obsolete"
+sbatch scripts/05_indel_realignment.sh
+
+
+#[] 6 SNP and genotype likelihood calling
+## To alter in this slurm script:
+## 		-
+echo "ANGSD to get the SNP calls and genotype likelihoods"
+echo "these are generated on a per snp basis"
+sbatch scripts/07_run_pcangsd.sh
+
